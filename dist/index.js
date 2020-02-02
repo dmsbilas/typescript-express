53aec4bd3b295da2263951e46204cf1dc15a6a3c
Bilas Siddiq <dmsbilas@yahoo.com>
2020-02-03 01:10:47 +0600

    adding routing and controller sections

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
-const port = 8080; // default port to listen
+const port = 3000; // default port to listen
 // define a route handler for the default home page
 app.get("/", (req, res) => {
     res.send("Hello world!");
